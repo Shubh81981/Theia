6acedf3d5442e7c6d526fa029b508dd8a267ed1f
shubh819811 <shubham05th@gmail.com>
2022-01-29 12:01:06 +0000

    model Migration

--- final_template/onlinecourse/models.py
+++ final_template/onlinecourse/models.py
@@ -94,6 +94,29 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+class Question(models.Model):
+    question_text=models.TextField()
+    Question_hint=models.TextField()
+    gradepoint=models.IntegerField()
+    Lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(
+            is_correct=True, id__in=selected_ids).count()
+            if all_answers == selected_correct:
+                return True    
+            else:
+                return False
+class Choice(models.Model):
+    question_text=models.TextField()
+    IsCorrect=models.BooleanField(default=False)
+    Question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   chocies = models.ManyToManyField(Choice)
+
+
 
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
